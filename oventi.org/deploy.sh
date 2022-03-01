f78bcb4b9b7abfc9780a8bba2dc900ab53b971a1
Andrés Proaño Valencia <andres@oventi.org>
2022-03-02 02:34:31 +1300

    added deploy script

--- oventi.org/deploy.sh
+++ oventi.org/deploy.sh
@@ -2,14 +2,8 @@
 
 # deploy the website to Catalyst Cloud's object container oventi.org
 
-export GIT_SSH_COMMAND="ssh -i ~/pk/oventi.net"
-
-cd ~/repo/websites/oventi.org/dist
-rm * -Rf
-cd ~/repo/websites/oventi.org
-git checkout main
-git pull
-yarn install
 yarn build
-cd ~/repo/websites/oventi.org/dist
-swift upload oventi.org *
+
+yarn cc containers --name oventi.org --empty
+
+yarn cc containers --name oventi.org --put-files ./dist

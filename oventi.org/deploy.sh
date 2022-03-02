c32d6f8a9a6f4cf4520cec30e8c94162c6ad0343
Andrés Proaño Valencia <andres@oventi.org>
2022-03-03 00:45:57 +1300

    update deployment script

--- oventi.org/deploy.sh
+++ oventi.org/deploy.sh
@@ -2,8 +2,10 @@
 
 # deploy the website to Catalyst Cloud's object container oventi.org
 
+BUCKET=https://object-storage.nz-hlz-1.catalystcloud.io/oventi.org
+
 yarn build
 
-yarn cc containers --name oventi.org --empty
+yarn cc containers --bucket $BUCKET --empty
 
-yarn cc containers --name oventi.org --put-files ./dist
+yarn cc containers --bucket $BUCKET --put-files ./dist

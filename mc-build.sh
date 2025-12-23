a6e8a52c5c79a52f30ba1d34b264726ff77404e5
Andrés Proaño Valencia <andres@oventi.org>
2025-12-23 07:23:27 +0000

    use mustache

---
+++ mc-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+TS=$(date +%Y%m%d)
+#mv ./multiculturalcollective.nz/index.html "./multiculturalcollective.nz/archive/index_$TS.html"
+
+yarn -s mustache \
+-p ./multiculturalcollective.nz/templates/head.mustache \
+-p ./multiculturalcollective.nz/templates/nav.mustache \
+-p ./multiculturalcollective.nz/templates/footer.mustache \
+./multiculturalcollective.nz/data.json ./multiculturalcollective.nz/templates/index.mustache > ./multiculturalcollective.nz/index.html

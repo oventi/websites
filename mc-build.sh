fc2a2f271f7297d6f7ae5a765bd8287ad1bae576
Andrés Proaño Valencia <andres@oventi.org>
2025-12-29 00:30:35 +0000

    updated the website with new thumnails and data

--- mc-build.sh
+++ mc-build.sh
@@ -5,6 +5,7 @@ TS=$(date +%Y%m%d)
 
 yarn -s mustache \
 -p ./multiculturalcollective.nz/templates/head.mustache \
+-p ./multiculturalcollective.nz/templates/social.mustache \
 -p ./multiculturalcollective.nz/templates/nav.mustache \
 -p ./multiculturalcollective.nz/templates/footer.mustache \
 ./multiculturalcollective.nz/data.json ./multiculturalcollective.nz/templates/index.mustache > ./multiculturalcollective.nz/index.html
